77082d1607f6c227b9e7811e36dc77a4018519b2
sailakshmy <sailakshmy94@gmail.com>
2023-10-09 16:25:37 +0530

    Commit-10:- Segregated the todo list into 2 columns

--- typescript-todo-app/src/components/TodoList.tsx
+++ typescript-todo-app/src/components/TodoList.tsx
@@ -6,8 +6,22 @@ import TodoCard from './TodoCard';
 
 const TodoList:FC<TodoListProps> = ({todoList, setTodoList}) => {
   return (
-    <div className='todoList'>
+    // <div className='todoList'>
+    //   
+    // </div>
+    <div className="container">
+      <div className="todoList">
+        <span className="todoList_sectionHeading">
+          Active Tasks
+        </span>
+        {todoList.map((todo)=><TodoCard todo={todo} key={todo.id} todoList={todoList} setTodoList={setTodoList}/>)}
+      </div>
+      <div className="todoList remove">
+      <span className="todoList_sectionHeading">
+          Completed Tasks
+        </span>
       {todoList.map((todo)=><TodoCard todo={todo} key={todo.id} todoList={todoList} setTodoList={setTodoList}/>)}
+      </div>
     </div>
   )
 }
