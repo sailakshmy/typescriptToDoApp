0de41ed0b4d417e23fa2428da671d81499c1fbc6
sailakshmy <sailakshmy94@gmail.com>
2023-10-09 12:33:35 +0530

    Commit-4:- Added the logic to add tasks to the todolist

--- typescript-todo-app/src/App.tsx
+++ typescript-todo-app/src/App.tsx
@@ -1,12 +1,30 @@
-import React,{FC} from 'react';
+import React,{FC, useState, FormEvent} from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './types';
 
 const App:FC =() => {
+  const [todo, setTodo] = useState<string>("");
+  // console.log("Todo:-", todo);
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const handleTodoToList = (e: FormEvent) => {
+    e.preventDefault();
+    if(todo){
+      setTodoList([...todoList, {
+        id: Date.now(),
+        todo,
+        isDone: false,
+      }]);
+      setTodo("");
+    }
+
+  }
+  // console.log("TodoList:-", todoList);
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleTodoToList={handleTodoToList}/>
     </div>
   );
 }
--- typescript-todo-app/src/components/InputField.tsx
+++ typescript-todo-app/src/components/InputField.tsx
@@ -1,10 +1,20 @@
-import React,{FC} from 'react';
+import React,{FC, FormEvent} from 'react';
 import './InputField.css';
 
-const InputField:FC =()=> {
+interface InputFieldProps {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleTodoToList: (e: FormEvent)=> void;
+}
+const InputField:FC<InputFieldProps> =({todo, setTodo, handleTodoToList})=> {
   return (
-    <form className='input'>
-      <input type="text" className="input_box" placeholder='Enter a task'/>
+    <form className='input' onSubmit={handleTodoToList}>
+      <input 
+        type="text" 
+        className="input_box"
+        placeholder='Enter a task'
+        value={todo}
+        onChange={(e)=> setTodo(e.target.value)}/>
       <button type="submit" className="input_submit">Go</button>
     </form>
   )
