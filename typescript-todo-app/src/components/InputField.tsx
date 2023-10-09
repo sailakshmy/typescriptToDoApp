b46a7c2d02b46b703d5324b058e2aeb8d631c4ed
sailakshmy <sailakshmy94@gmail.com>
2023-10-09 12:43:08 +0530

    Commit-5: Added ref to remove the focus from the input box after a task has been added to the list

--- typescript-todo-app/src/components/InputField.tsx
+++ typescript-todo-app/src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React,{FC, FormEvent} from 'react';
+import React,{FC, FormEvent, useRef} from 'react';
 import './InputField.css';
 
 interface InputFieldProps {
@@ -7,14 +7,20 @@ interface InputFieldProps {
     handleTodoToList: (e: FormEvent)=> void;
 }
 const InputField:FC<InputFieldProps> =({todo, setTodo, handleTodoToList})=> {
+  const inputBoxRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = (e: FormEvent) => {
+    handleTodoToList(e);
+    inputBoxRef.current?.blur();
+  }
   return (
-    <form className='input' onSubmit={handleTodoToList}>
+    <form className='input' onSubmit={handleSubmit}>
       <input 
         type="text" 
         className="input_box"
         placeholder='Enter a task'
         value={todo}
-        onChange={(e)=> setTodo(e.target.value)}/>
+        onChange={(e)=> setTodo(e.target.value)}
+        ref={inputBoxRef}/>
       <button type="submit" className="input_submit">Go</button>
     </form>
   )
